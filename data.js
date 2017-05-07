7c2f68dd9c7da8fa49f7aec8a8e5591197b87b05
tinfelise <tinfelise@gmail.com>
2017-05-07 11:36:43 -0700

    Removed fake events

--- data.js
+++ data.js
@@ -20,12 +20,13 @@ var events = [
 		'location':'1',
 		'start': 'May 13 2017 13:00:00 PDT',
 		'end': 'May 13 2017 16:00:00 PDT'
-	},
-	{
-		'location':'1',
-		'start': 'Jun 10 2017 08:00:00 PDT',
-		'end': 'Jun 10 2017 15:00:00 PDT'
 	}
+	// ,
+	// {
+	// 	'location':'1',
+	// 	'start': 'Jun 10 2017 08:00:00 PDT',
+	// 	'end': 'Jun 10 2017 15:00:00 PDT'
+	// }
 ];
 
 var menu = [
